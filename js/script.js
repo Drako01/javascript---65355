bb85a77c71361e86b3a5c078d6e6bebd7958bdcb
Alejandro Di Stefano <addistefano@live.com.ar>
2024-11-19 20:56:32 -0300

    Clase 01

---
+++ js/script.js
@@ -0,0 +1,55 @@
+// console.log("Hola Mundo.!!");
+
+// ///var nombreDeUnAlumno; // Declare una variable
+// let nombreDeUnAlumno;
+// nombreDeUnAlumno = "Alejandro"; // Asignarle un Valor / Inicializarla
+// console.log(typeof nombreDeUnAlumno); // string
+
+// //var nombreDeUnAlumno = "Mauro"; // Declaro una variable Nueva y la inicializo
+// let nombreDeUnAlumno2 = "Mauro";
+// nombreDeUnAlumno2 = "Matias"; // String
+// nombreDeUnAlumno2 = 123; // Number
+// nombreDeUnAlumno2 = true; // Boolean
+// console.log(typeof nombreDeUnAlumno2); // boolean
+
+// const numeroA = 12;
+// // numeroA = 123456; // Las constantes no permiten reasignar valores
+// console.log( typeof numeroA); // number
+// let saludo = "Hola";
+// let nombreDeUnAlumno = "Mauro";
+
+// console.log(saludo + " " + nombreDeUnAlumno + ".!!!"); // Usar el + para concatenar
+
+// let numeroA = 10;
+// let numeroB = 5;
+// // console.log(numeroA);
+// console.log("Suma: " + (numeroA + numeroB)); // Operacion de Suma
+// console.log("Resta: " + (numeroA - numeroB));
+// console.log("Multiplicacion: " + (numeroA * numeroB));
+// console.log("Division: " + (numeroA / numeroB));
+
+// prompt + alert + confirm
+
+// let nombreDeUsuario = prompt("Ingrese su nombre: ");
+// let edadDelUsuario = prompt("Ingrese su edad: ");
+// //console.log(nombreDeUsuario);
+// alert("El nombre ingresado es: " + nombreDeUsuario);
+// alert("La edad ingresada es: " + edadDelUsuario);
+
+// confirm("¿Esta seguro que quiere cerrar la Aplicacion?");
+
+
+// Calculadora
+// let numeroA = parseInt(prompt("Ingrese el 1er numero"));
+// let numeroB = parseInt(prompt("Ingrese el 2do Numero"));
+
+// let resultado = numeroA + numeroB;
+// console.log(numeroA); // NaN
+// console.log(numeroB);
+// console.log(resultado);
+// console.log( typeof numeroA); // Tipo Number
+// console.log( typeof numeroB);
+// console.log( typeof resultado);
+// // alert(resultado);
+// alert("El resultado es: " + resultado);
+

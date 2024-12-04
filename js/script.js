cd27273f8d2df810d4b79e79d6d457b6bdca48a6
Alejandro Di Stefano <addistefano@live.com.ar>
2024-12-03 21:04:51 -0300

    Clase 03

--- js/script.js
+++ js/script.js
@@ -1,196 +1,273 @@
-// let verdadero = true;
-// console.log(typeof verdadero); // Tipo de dato
+// Funciones
+
+// const numeroA = 10;
+// const numeroB = 12;
+
+// const resultado = numeroA + numeroB;
+// console.log("Felicitaciones, el resultado es " + resultado);
 
 
-// Estructtura de un if
-// if(condicion){ // si condicion es true, entonces se  ejecuta esto!
-//     ejecuta esto!
+// const numeroC = 1025;
+// const numeroD = 412;
+
+// const resultado1 = numeroC + numeroD;
+// console.log("Felicitaciones, el resultado es " + resultado1);
+
+// const numeroC1 = 1335;
+// const numeroD1 = 4912;
+
+// const resultado11 = numeroC1 + numeroD1;
+// console.log("Felicitaciones, el resultado es " + resultado11);
+
+/// Declarar la Funcion
+
+// Variables Globales
+// const numeroA = parseInt(prompt("Ingrese el 1er Numero"));
+// const numeroB = parseInt(prompt("Ingrese el 2do Numero"));
+// const resultado = numeroA + numeroB;
+
+// function sumar() {
+//     // Variables Locales
+//     const numeroA = parseInt(prompt("Ingrese el 1er Numero"));
+//     const numeroB = parseInt(prompt("Ingrese el 2do Numero"));
+//     const resultado = numeroA + numeroB;
+
+//     console.log("Felicitaciones, el resultado de la Suma es " + resultado);
 // }
 
-// if (verdadero) {
-//     console.log("El valor de verdad de verdadero es " + verdadero);
-// } else {
-//     console.log("El valor de verdad de verdadero es " + verdadero + " se ejecuto desde el else");
+// // for(let i = 0; i < 5; i++){
+// //     sumar();
+// // }
+
+// function restar() {
+//     // Variables Locales
+//     const numeroA = parseInt(prompt("Ingrese el 1er Numero"));
+//     const numeroB = parseInt(prompt("Ingrese el 2do Numero"));
+//     const resultado = numeroA - numeroB;
+
+//     console.log("Felicitaciones, el resultado de la Resta es " + resultado);
+// }
+// function multiplicar() {
+//     // Variables Locales
+//     const numeroA = parseInt(prompt("Ingrese el 1er Numero"));
+//     const numeroB = parseInt(prompt("Ingrese el 2do Numero"));
+//     const resultado = numeroA * numeroB;
+
+//     console.log("Felicitaciones, el resultado de la Multiplicacion es " + resultado);
 // }
 
-// let stock = false;
-
-// if (stock) {
-//     console.log("Usted puede comprar");
-// } else {
-//     console.log("No hay mas Stock");
-// }
-
-// let edad = parseInt(prompt("Ingrese su Edad, No mienta.!!"));
-
-// const edadDeReferencia = 18;
-
-// if (edad >= edadDeReferencia){
-//     console.log("Usted puede Pasar... Bienvenido!!");
-// } else {
-//     console.error("MENOR DE EDAD.!!! Usted NO puede Pasar... ");
-// }
-
-// let numero = parseInt(prompt("Ingrese un Número cualquiera.!!"));
-// console.log(numero);
-
-// if (numero > 0) {
-//     alert("Usted ingreso un numero positivo");
-// } else if (numero < 0) {
-//     alert("Usted ingreso un numero negativo");
-// } else if (numero === 0) {
-//     alert("Usted ingreso el Cero");
-// } else if (isNaN(numero)){
-//     alert("ERROR, Ingrese un Número Válido, no escriba un Texto")
-// } else {
-//     alert("ERROR, no ha ingresado Nada.!")
-// }
-
-// let numeroA = 12; // Number
-// let numeroB = "12"; // String
-
-// if ( numeroA == numeroB ) {
-//     console.log("Ambos numeros son Iguales.!!");
-// } else {
-//     console.error("Los numeros son Distintos.!!");
-// }
-
-// if ( numeroA === numeroB ) {
-//     console.log("Ambos numeros son Estrictamente Iguales.!!");
-// } else {
-//     console.error("Los numeros son Estrictamente Distintos.!!");
-// }
-
-// let numeroA = 1;
-// let numeroB = 10;
-
-// let numero = parseInt(prompt("Ingrese un numero cualquiera"));
-
-// // Si el numero esta entre numeroA y numeroB
-// if (numero > numeroA && numero < numeroB) {
-//     console.log("El numero ingresado esta entre " + numeroA + " y " + numeroB);
-// }
-
-
-// let verdadero = false;
-
-// if (!verdadero) { // Porque la negacion de el false es el true
-//     console.log("Verdadero es Falso");
-// }
-
-// Ciclos
-// Ciclo por condicion
-
-// while
-
-// let nombreDelUsuario = prompt("Ingrese su nombre");
-
-// // Necesito validar que el Usuario ingrese un String
-// console.log(nombreDelUsuario);
-
-// while(nombreDelUsuario === '' || !isNaN(nombreDelUsuario)) {
-//     alert("Ingrese un Nombre valido");
-//     nombreDelUsuario = prompt("Ingrese su nombre");
-// }
-// alert("Su Nombre es: " + nombreDelUsuario);
-
-
-
-
-// let nombreDelUsuario;
-// let validacion;
-
-// do {
-//     nombreDelUsuario = prompt("Ingrese su nombre");
-//     validacion = nombreDelUsuario === '' || !isNaN(nombreDelUsuario); // es boolean
-
-//     if(validacion){
-//         alert("Ingrese un Nombre valido");
+// function dividir() {
+//     // Variables Locales
+//     const numeroA = parseInt(prompt("Ingrese el 1er Numero"));
+//     const numeroB = parseInt(prompt("Ingrese el 2do Numero"));
+//     const resultado = numeroA / numeroB;
+//     if(numeroB !== 0 ) {
+//         console.log("Felicitaciones, el resultado la Division es " + resultado);
+//     } else {
+//         console.error("Error, No se puede dividir por 0");
 //     }
-// } while (validacion);
+// }
 
-// alert("Su Nombre es: " + nombreDelUsuario);
+// /// Invocacion de las Funciones
+// sumar();
+// restar();
+// multiplicar();
+// dividir();
 
-// Ciclos por conteo
+// Funciones con parametros
 
-// Ciclo for
+// Variables Globales
+// let numeroA = parseFloat(prompt("Ingrese el 1er Numero"));
+// let numeroB = parseFloat(prompt("Ingrese el 2do Numero"));
+//let operacion = prompt("Ingrese la Operacion: (+, -, * , /)");
 
-// for (let i = 0; i < 5; i++) {
-//     console.log("Me estoy ejecutando " + (i + 1) + " veces.!");
+
+// function validarResultado(p) { // retorna un Booleano
+//     if (isNaN(p)) {
+//         console.error("Error, Ingrese un numero valido");
+//         return false;
+//     } else {
+//         return true;
+//     }
+// }
+
+// function sumar(a, b){ // a y b son los parametros de la funcion
+//     resultado = a + b;
+//     console.log("Felicitaciones, el resultado de la Suma es " + resultado);
+// }
+// function restar(a, b){ // a y b son los parametros de la funcion
+//     resultado = a - b;
+//     console.log("Felicitaciones, el resultado de la Resta es " + resultado);
+// }
+// function multiplicar(a, b){ // a y b son los parametros de la funcion
+//     resultado = a * b;
+//     console.log("Felicitaciones, el resultado de la Multiplicacion es " + resultado);
+// }
+// function dividir(a, b){ // a y b son los parametros de la funcion
+//     if(b === 0) {
+//         console.error("Error, No se puede dividir por 0");
+//     } else {
+//         resultado = a / b;
+//         console.log("Felicitaciones, el resultado de la Division es " + resultado);
+//     }
+// }
+
+// if(validarResultado(numeroA) && validarResultado(numeroB)){
+//     dividir(numeroA, numeroB); // Paso los Argumentos
+// } else {
+//     console.error("Error, Operacion cancelada debido a entradas invalidas.!")
 // }
 
 
-// Tabla de Verdad (logica proposiocional)
+// function calculadora(a, b, operacion) {
 
-// const V = true;
-// const F = false;
+//     if (validarResultado(a) && validarResultado(b)) {
+//         let resultado;
+//         switch (operacion) {
+//             case "+":
+//                 resultado = a + b;
+//                 console.log("Felicitaciones, el resultado de la Suma es " + resultado);
+//                 break;
+//             case "-":
+//                 resultado = a - b;
+//                 console.log("Felicitaciones, el resultado de la Resta es " + resultado);
+//                 break;
+//             case "*":
+//                 resultado = a * b;
+//                 console.log("Felicitaciones, el resultado de la Multiplicacion es " + resultado);
+//                 break;
+//             case "/":
+//                 if (b === 0) {
+//                     console.error("Error, No se puede dividir por 0");
+//                 } else {
+//                     resultado = a / b;
+//                     console.log("Felicitaciones, el resultado de la Division es " + resultado);
+//                 }
+//                 break;
+//             default:
+//                 console.error("Error, Operacion no valida.!")
+//         }
 
-// console.log("Tabla de Verdad del Operador && (Y)");
-// console.log(V && V); // Si ambos son V => true
-// console.log(V && F); // Si al menos uno de los valores es F => false
-// console.log(F && V); // Si al menos uno de los valores es F => false
-// console.log(F && F); // Si ambos de los valores son F => false
+//     } else {
+//         console.error("Error, Operacion cancelada debido a entradas invalidas.!")
+//     }
+// }
+
+// calculadora(numeroA, numeroB, operacion);
 
 
-// console.log("Tabla de Verdad del Operador || (O)");
-// console.log(V || V); // Si ambos son V => true
-// console.log(V || F); // Si al menos uno de los valores es V => true
-// console.log(F || V); // Si al menos uno de los valores es V => true
-// console.log(F || F); // Si ambos de los valores son F => false
 
-// console.log("Tabla de Verdad del Operador de Negación (!)");
-// console.log(!V); // Si no es Verdadero => false
-// console.log(!F); // Si no es False => true
+// let numeroA = parseFloat(prompt("Ingrese el 1er Numero"));
+// let numeroB = parseFloat(prompt("Ingrese el 2do Numero"));
 
-//39. Pide al usuario que ingrese su mes de nacimiento y muestra su 
-//    signo zodiacal en la consola.
+// function sumar(a, b) {
+//     return console.log("Felicitaciones, el resultado de la Suma es " + (a + b)); // String
+// }
 
-let mes;
+// function sumar(a, b){
+//     return a + b; // Numero
+// }
+// console.log(sumar(numeroA, numeroB));
+
+
+// const sumar = function (a , b) {
+//     return a + b;
+// }
+
+// console.log(sumar(numeroA, numeroB));
+
+// const saludo = function(nombre){
+//     return "Hola " + nombre + ", como estas?";
+// }
+
+// console.log(saludo("Alejandro"));
+
+// Funciones Flecha (Retorno implisito)
+// const sumar = (a, b) => {
+//     return a + b;
+// }
+// const sumar = (a, b) => {
+//     const resultado = a + b 
+//     console.log(resultado)
+// }; // (Retorno implisito)
+
+// sumar(10, 12);
+
+// const sumar = (a, b) => a + b; // (Retorno implisito)
+
+// console.log(sumar(10, 12));
+
+
+
+
+
+// calculadora(numeroA, numeroB, operacion);
+
+let continuar;
+
 do {
-    mes = parseInt(prompt("Ingrese su mes de Nacimiento (1-12)"));
-    if (isNaN(mes) || mes < 1 || mes > 12) {
-        alert("Por favor, ingrese un Número del 1 al 12");
+    let numeroA = parseFloat(prompt("Ingrese el 1er Numero"));
+    let numeroB = parseFloat(prompt("Ingrese el 2do Numero"));
+    let operacion = prompt("Ingrese la Operacion: (+, -, * , /)");
+
+    function validarResultado(p) { // retorna un Booleano
+        if (isNaN(p)) {
+            console.error("Error, Ingrese un numero valido");
+            return false;
+        } else {
+            return true;
+        }
     }
 
-} while (isNaN(mes) || mes < 1 || mes > 12);
+    function calculadora(a, b, operacion) {
 
-switch (mes) {
-    case 1:
-        console.log("Tu signo sodiacal es Capricornio o Acuario");
-        break;
-    case 2:
-        console.log("Tu signo sodiacal es Acuario o Pisis");
-        break;
-    case 3:
-        console.log("Tu signo sodiacal es Pisis o Aries");
-        break;
-    case 4:
-        console.log("Tu signo sodiacal es Aries o Tauro");
-        break;
-    case 5:
-        console.log("Tu signo sodiacal es Tauro o Géminis");
-        break;
-    case 6:
-        console.log("Tu signo sodiacal es Géminis o Cáncer");
-        break;
-    case 7:
-        console.log("Tu signo sodiacal es Cáncer o Leo");
-        break;
-    case 8:
-        console.log("Tu signo sodiacal es Leo o Virgo");
-        break;
-    case 9:
-        console.log("Tu signo sodiacal es Virgo o Libra");
-        break;
-    case 10:
-        console.log("Tu signo sodiacal es Libra o Escorpio");
-        break;
-    case 11:
-        console.log("Tu signo sodiacal es Escorpio o Sagitario");
-        break;
-    case 12:
-        console.log("Tu signo sodiacal es Sagitario o Capricornio");
-        break;
-    default:
-        console.log("Por favor, ingrese un numero valido del 1 al 12");
-}
+        if (validarResultado(a) && validarResultado(b)) {
+            let resultado;
+            switch (operacion) {
+                case "+":
+                    resultado = a + b;
+                    console.log("Felicitaciones, el resultado de la Suma es " + resultado);
+                    break;
+                case "-":
+                    resultado = a - b;
+                    console.log("Felicitaciones, el resultado de la Resta es " + resultado);
+                    break;
+                case "*":
+                    resultado = a * b;
+                    console.log("Felicitaciones, el resultado de la Multiplicacion es " + resultado);
+                    break;
+                case "/":
+                    if (b === 0) {
+                        console.error("Error, No se puede dividir por 0");
+                    } else {
+                        resultado = a / b;
+                        console.log("Felicitaciones, el resultado de la Division es " + resultado);
+                    }
+                    break;
+                default:
+                    console.error("Error, Operacion no valida.!")
+            }
+
+        } else {
+            console.error("Error, Operacion cancelada debido a entradas invalidas.!")
+        }
+    }
+
+    calculadora(numeroA, numeroB, operacion);
+
+    
+//     do {
+//         continuar = prompt("Desea continuar (s/n)").toLowerCase();
+//         if(continuar !== 's' && continuar !== 'n'){
+//             console.error("Error, Entrada no valida. Por favor ingrese 's' o 'n'");
+//         }
+
+//     } while (continuar !== 's' && continuar !== 'n');
+    
+// } while (continuar === "s");
+
+    continuar = confirm("Desea realizar otra Operacion?")
+} while (continuar);
+
+console.log("Gracias por usar la calculadora");
+
